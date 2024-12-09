1cf477291358ee4b88bea3dc7bb7d97719a45b3e
sabilar-rahman <sabilarrahman36@gmail.com>
2024-12-09 07:15:19 +0600

    production : demo admin color change

--- src/pages/AuthPage/Login.tsx
+++ src/pages/AuthPage/Login.tsx
@@ -93,7 +93,7 @@ const Login = () => {
       <div className="flex justify-between gap-4 mt-4">
         <button
           onClick={handleDemoAdmin}
-          className="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900"
+          className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
         >
           Demo Admin
         </button>
